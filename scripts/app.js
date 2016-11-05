ceb656ace71f1877c350717c2aae78716085a132
dkingdon <daniel.kingdon@gmail.com>
2016-11-05 00:08:17 -0700

    both game pieces are working now upon key press. Encountered strange bug that will need to be fixed after core functionality is complete. See comments on app.js line 28

--- scripts/app.js
+++ scripts/app.js
@@ -12,19 +12,24 @@ var keyCountTop = 0;
           console.log(keyCountTop); //Logging in console for testing. Will remove once finished.
       }
           /* ---- Negative penalty if player hits wrong key ---- */
-      else if (e.which === 65 || e.which === 87 || e.which === 69 || e.which === 68 || e.which === 88 || e.which === 90) {
-        $( ".blockOne" ).animate({ "left": "-=30px" }, 80 );
-      }
+      // else if (e.which === 65 || e.which === 87 || e.which === 69 || e.which === 68 || e.which === 88 || e.which === 90) {
+      //   $( ".blockOne" ).animate({ "left": "-=30px" }, 80 );
+      // }
     });
 
     /* ---- Bottom Player animate code ----*/
-    $( "#rightTwo" ).click(function() {
-    $( ".blockTwo" ).animate({ "left": "+=15px" }, 80 );
+var keyCountBottom = 0;
+    $( "body" ).on('keydown', function(e) {
+      if (e.which === 75){
+        $( ".blockTwo" ).animate({ "left": "+=15px" }, 80 );
+          keyCountBottom += 1;
+          console.log(keyCountBottom); //Logging in console for testing. Will remove once finished.
+      }
+      /* ---- Commenting out until i figure out bug. This is causing the game to break*/
+      // else if (e.which === 74 || e.which === 73 || e.which === 79 || e.which === 76 || e.which === 188 || e.which === 77); {
+      //   $( ".blockTwo" ).animate({ "left": "-=30px" }, 80 );
+      // }
     });
-          /* Code to move player left */
-          // $( "#leftTwo" ).click(function(){
-          // $( ".blockTwo" ).animate({ "left": "-=15px" }, 80 );
-          // });
 
 });
 
@@ -34,6 +39,8 @@ var keyCountTop = 0;
 // Keys s is 83 | k is 75
 // will want to set winning number of clicks based on browser window width
 //       http://stackoverflow.com/questions/1038727/how-to-get-browser-width-using-javascript-code
+//
+// getting var data to persist after page load. For win counter: http://stackoverflow.com/questions/16206322/how-to-get-js-variable-to-retain-value-after-page-refresh
 
 // need a listener to identify when a button is pressed. Maybe add to a counter, ever 10 presses can be one button push? will need function for that as well. every 10, counter starts over?
 
