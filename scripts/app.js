7a85df686a22fc8097facc8c7b4eeeeefe7627b9
dkingdon <daniel.kingdon@gmail.com>
2016-11-04 17:45:30 -0700

    initial race animation working

---
+++ scripts/app.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+// Testing jquery.animate property
+    // NOT WORKING //
+// $( "#right" ).click(function() {
+//   $( ".block" ).animate({ "right": "+=50px" }, "slow" );
+// });
+//
+// $( "#left" ).click(function(){
+//   $( ".block" ).animate({ "left": "-=50px" }, "slow" );
+// });
+
+console.log("Hey, I'm connected");
+
+
+});
