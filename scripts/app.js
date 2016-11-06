9521617a5d6f77e6551afe4314618a3586b1d3b1
dkingdon <daniel.kingdon@gmail.com>
2016-11-05 21:28:39 -0700

    hud style and placement fine tuning and added win conditions

--- scripts/app.js
+++ scripts/app.js
@@ -5,11 +5,16 @@ $(document).ready(function() {
     /* ---- Top Player animate code ----*/
 var keyCountTop = 0;
     $( 'body' ).on('keydown', function(e) {
-      if (e.which === 83){
+      if (e.which === 83 && keyCountTop <= 104){
         $( '.blockOne' ).animate({ 'left': '+=15px' }, 80 );
           /* counts key is presses. As of now, im planning to use this number to determine winner*/
           keyCountTop += 1;
+            /* Alert winner */
+            if (keyCountTop === 104 && keyCountBottom < 104){
+              alert('Player One Wins!')
+            }
           console.log(keyCountTop); //Logging in console for testing. Will remove once finished.
+
       }
           /* ---- Negative penalty if player hits wrong key ---- */
       // else if (e.which === 65 || e.which === 87 || e.which === 69 || e.which === 68 || e.which === 88 || e.which === 90) {
@@ -20,16 +25,22 @@ var keyCountTop = 0;
     /* ---- Bottom Player animate code ----*/
 var keyCountBottom = 0;
     $( 'body' ).on('keydown', function(e) {
-      if (e.which === 75){
+      if (e.which === 75 && keyCountBottom <= 104){
         $( '.blockTwo' ).animate({ 'left': '+=15px' }, 80 );
           keyCountBottom += 1;
           console.log(keyCountBottom); //Logging in console for testing. Will remove once finished.
+            /* Alert winner */
+            if (keyCountBottom === 104 && keyCountTop < 104){
+              alert('Player Two Wins!')
+            }
       }
       /* ---- Commenting out until i figure out bug. This is causing the game to break*/
       // else if (e.which === 74 || e.which === 73 || e.which === 79 || e.which === 76 || e.which === 188 || e.which === 77); {
       //   $( ".blockTwo" ).animate({ "left": "-=30px" }, 80 );
       // }
     });
+
+ // WINNER: 103 key presses
 
 });
 
